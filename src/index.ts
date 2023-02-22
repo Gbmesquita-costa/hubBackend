8ccc5b1c067557bf49eebeabbd3c12945e3f6844
gabriel <mesquitag313@gmail.com>
2023-02-22 17:52:01 -0300

    OriginChanged

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,8 @@ app.use(express.text())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cors({
-    credentials: true
+    credentials: true,
+    origin: "https://hub-dashboard.vercel.app/"
 }))
 
 app.use(routes)
